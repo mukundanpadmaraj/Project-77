ba58daa61109b81aa0c3d73adf88916f17c6716c
mukundanpadmaraj <91674444+mukundanpadmaraj@users.noreply.github.com>
2022-01-23 09:50:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var first_name="Mukundan"
+var last_name="Padmaraj"
+console.log(first_name)
+console.log(last_name)
+var full_name=first_name.concat(last_name)
+console.log(full_name)
+
+var a=87451
+var b=12938
+var r=a+b
+console.log(r)
+
+var x=a-b
+console.log(x)
+var u=a*b
+console.log(u)
+var y=a/b
+console.log(y)
